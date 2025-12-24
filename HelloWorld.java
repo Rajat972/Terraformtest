72b5c8b0a1fb03f4502da6d9371304562e4168a8
Rajat Bandhral <105308791+Rajat972@users.noreply.github.com>
2025-12-24 23:35:07 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,1 @@
-package com.example;
 
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World - Jenkins Maven Build Successful!");
-    }
-}
